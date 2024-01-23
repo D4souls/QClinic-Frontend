d9b0104d2685631ea4a927da241cf73d831047ff
Sam <sergioavilamacho12@gmail.com>
2024-01-23 12:44:34 +0100

    Configure routes to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserDataComponent } from './user-data/user-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'home', title: 'Home page', component: HomeComponent}, {path: 'user-data', title: 'Personal information', component: UserDataComponent}];
