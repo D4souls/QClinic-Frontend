6b4859a2cb09e80e630f6d8fd0007be3f447aad1
D4souls <sergioavilamacho12@gmail.com>
2024-01-23 20:36:06 +0100

    Create get API data from server

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  name = "Sergio"
+  name: string = "";
+
+  httpClient = inject(HttpClient);
+  data: any = {};
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.httpClient.get('http://localhost/api/example.json')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.data = data;
+        this.name = data[0].firstname;
+      });
+  };
 }
