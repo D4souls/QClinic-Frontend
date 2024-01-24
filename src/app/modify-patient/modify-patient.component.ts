7f8818689282088da196f18e1ad9fe036b7e4e54
D4souls <sergioavilamacho12@gmail.com>
2024-01-24 20:23:30 +0100

    Create modify-patient component

---
+++ src/app/modify-patient/modify-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-patient',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-patient.component.html',
+  styleUrl: './modify-patient.component.css'
+})
+export class ModifyPatientComponent {
+
+}
