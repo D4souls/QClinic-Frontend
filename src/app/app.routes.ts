408a0e0412fe8503523b18932db74fd3bc55ec4e
D4souls <sergioavilamacho12@gmail.com>
2024-01-24 20:04:36 +0100

    Add new route to doctors and patients component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserDataComponent } from './user-data/user-data.component';
+import { PatientsComponent } from './patients/patients.component';
+import { DoctorsComponent } from './doctors/doctors.component';
 
-export const routes: Routes = [{ path: 'home', title: 'Home page', component: HomeComponent}, {path: 'user-data', title: 'Personal information', component: UserDataComponent}];
+export const routes: Routes = [
+  { path: 'home', title: 'Home page', component: HomeComponent },
+  {
+    path: 'user-data',
+    title: 'Personal information',
+    component: UserDataComponent,
+  },
+  {
+    path: 'patients',
+    title: 'Manage patients',
+    component: PatientsComponent,
+  },
+  {
+    path: 'doctors',
+    title: 'Manage doctors',
+    component: DoctorsComponent,
+  },
+
+];
