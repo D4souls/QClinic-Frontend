881966fd9610871fa4c462108d568f007114654c
Sam <sergioavilamacho12@gmail.com>
2024-01-26 13:52:06 +0100

    Demo charge data into component and simple UI

--- src/app/modify-patient/modify-patient.component.ts
+++ src/app/modify-patient/modify-patient.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModifyUserService } from '../service/modify-user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modify-patient',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './modify-patient.component.html',
   styleUrl: './modify-patient.component.css'
 })
-export class ModifyPatientComponent {
+export class ModifyPatientComponent implements OnInit{
+  
+  load: boolean = false;
 
+  constructor(public modifyUserService: ModifyUserService, private router: Router) {};
+
+  ngOnInit(): void {
+    console.log(this.modifyUserService.userDNI.length);
+    this.chekData();
+  }
+
+  chekData():void {
+    if(this.modifyUserService.userDNI.length > 0){
+      this.load = true;
+    } else {
+      this.router.navigate(['/patients']);
+    }
+  }
+
+  discardChanges():void{
+    this.router.navigate(['/patients']);
+  }
+
+  
 }
