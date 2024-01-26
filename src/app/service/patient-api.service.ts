6af0cfa2b034c08008b44a062b76a2a8e9d95db2
Sam <sergioavilamacho12@gmail.com>
2024-01-26 14:05:22 +0100

    Create service get patient data

---
+++ src/app/service/patient-api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientApiService {
+
+  constructor(private httpclient: HttpClient) { }
+
+  getPatientData(): Observable<any>{
+    return this.httpclient.get('http://localhost/api/patients.php');
+  }
+}
