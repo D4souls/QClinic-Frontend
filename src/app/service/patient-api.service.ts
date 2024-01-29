844d470ae9964a9ac00ab3435c6d18d2d52faf05
Sam <sergioavilamacho12@gmail.com>
2024-01-29 16:38:33 +0100

    Remove data storage from patient-api-service

--- src/app/service/patient-api.service.ts
+++ src/app/service/patient-api.service.ts
@@ -9,6 +9,8 @@ export class PatientApiService {
 
   constructor(private httpclient: HttpClient) { }
 
+  data: any = [];
+
   getPatientData(): Observable<any>{
     return this.httpclient.get('http://localhost/api/patients.php');
   }
