dc30679136e9b706656fd8453d2f63fe30f2390a
D4souls <sergioavilamacho12@gmail.com>
2024-01-31 19:49:38 +0100

    Add new routes to API

--- src/app/service/patient-api.service.ts
+++ src/app/service/patient-api.service.ts
@@ -9,9 +9,23 @@ export class PatientApiService {
 
   constructor(private httpclient: HttpClient) { }
 
-  data: any = [];
-
   getPatientData(): Observable<any>{
     return this.httpclient.get('http://localhost:8080/patients');
+  }
+
+  deletePatient(dni: string): Observable<any> {
+    return this.httpclient.delete(`http://localhost:8080/delete-patient/${dni}`);
+  }
+
+  createPatient(patientData: any): Observable<any> {
+    return this.httpclient.post('http://localhost:8080/create-patient', patientData);
+  }
+
+  modifyPatient(patientData: any): Observable<any> {
+    return this.httpclient.post('http://localhost:8080/modify-patient', patientData);
+  }
+
+  getDoctors(): Observable<any> {
+    return this.httpclient.get('http://localhost:8080/doctors')
   }
 }
