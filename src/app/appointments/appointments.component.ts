5fb48166dac6a20b5b4cf3ae204ed21c2f9e0346
D4souls <sergioavilamacho12@gmail.com>
2024-02-01 19:49:02 +0100

    Install pakages from fullcalendar

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core';
+import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-appointments',
@@ -13,18 +15,35 @@ import esLocale from '@fullcalendar/core/locales/es';
 })
 export class AppointmentsComponent {
 
+  calendarEvents: any = []
+
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',    
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
     weekNumbers: true,
     navLinks: true,
     firstDay: 1,
     nowIndicator: true,
     locale: esLocale,
     height: 650,
+    editable: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    themeSystem: 'standard',
     events: [
       { title: 'event 1', date: '2024-02-01' },
       { title: 'event 2', date: '2024-02-02' }
-    ]
+    ],
+    dayMaxEvents: true,
   }
+
+  createAppointment(){
+
+  }
+
+  
+
 }
