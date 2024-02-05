3275cb4b0f2320a11c31e3733400874171d57edf
Sam <sergioavilamacho12@gmail.com>
2024-02-05 18:41:09 +0100

    Function to create appointment

--- src/app/appointments/appointments.component.ts
+++ src/app/appointments/appointments.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal, effect, OnInit } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { Calendar, CalendarOptions } from '@fullcalendar/core';
+import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { PatientApiService } from '../service/patient-api.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointments',
   standalone: true,
-  imports: [FullCalendarModule],
+  imports: [FullCalendarModule, ReactiveFormsModule],
   templateUrl: './appointments.component.html',
-  styleUrl: './appointments.component.css'
+  styleUrl: './appointments.component.css',
 })
-export class AppointmentsComponent {
+export class AppointmentsComponent implements OnInit {
 
-  calendarEvents: any = []
+  constructor(private apiGetPatient: PatientApiService, private router: Router){}
+
+  calendarEvents: any[] = [];
+
+  ngOnInit(): void {
+    this.getAppointments();
+    setTimeout(() => {
+      this.calendarOptions = {
+        events: this.calendarEvents,
+      }
+    }, 800);
+  }
 
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',    
+    initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
     weekNumbers: true,
     navLinks: true,
@@ -30,20 +45,79 @@ export class AppointmentsComponent {
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
+
     themeSystem: 'standard',
-    events: [
-      { title: 'event 1', date: '2024-02-01' },
-      { title: 'event 2', date: '2024-02-02' }
-    ],
-    dayMaxEvents: true,
+    dayMaxEventRows: false,
+  };
+
+  handleDateClick(arg:any) {
+    alert('date click! ' + arg.dateStr)
   }
 
-  createAppointment(){
 
+  createAppointmentForm = new FormGroup({
+    patient: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required),
+    hour: new FormControl('', Validators.required),
+  })
+
+  createAppointment() {
+
+    const dataCreateAppointment = {
+      date: `${this.createAppointmentForm.value.date} ${this.createAppointmentForm.value.hour}`,
+      dniPatient: this.createAppointmentForm.value.patient,
+      dniDoctor: "72210584Z"
+    }
+
+    // console.log(dataCreateAppointment);
+
+    
+    this.apiGetPatient.createAppointments(dataCreateAppointment).subscribe((response: any) => {
+      if (response.success) {
+        Swal.fire({
+          title: 'Appointment created!',
+          showDenyButton: false,
+          confirmButtonText: 'Ok',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.reload();
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: response.message,
+        });
+      }
+    },
+    (error) => {
+      console.error('Error en la solicitud:', error.error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error.error.data,
+      });
+    })
+    
+    console.log(this.calendarEvents);
   }
 
-  
+  getAppointments(): void {
+    this.apiGetPatient.getAppointments().subscribe((data: any) => {
+      const dataEvent = data.data.map((appointments: any) => {
+        return {
+          title: `${appointments.patientFirstname} ${appointments.patientLastname}`,
+          start: appointments.date,
+        };
+      });
+      
+      this.calendarEvents = dataEvent;
 
+      console.log(dataEvent);
+
+    })
+  }
 }
