e659a221ca639c682cd5c875cdc225f2cc52f1d7
Sam <sergioavilamacho12@gmail.com>
2024-02-06 13:16:43 +0100

    Charge flowbite on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,11 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
   title = 'consultorio-medico';
+
+  ngOnInit(): void {
+      initFlowbite();
+  }
 }
