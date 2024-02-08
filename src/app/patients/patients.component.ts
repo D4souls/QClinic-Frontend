0c651abb544e4a95618bc50584aed64d1f4e2392
Sam <sergioavilamacho12@gmail.com>
2024-02-08 13:50:18 +0100

    Upgrade filter to search patients and rework route.navigate to modify-patient

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ApiService } from '../service/api.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-patients',
@@ -30,29 +31,57 @@ export class PatientsComponent implements OnInit {
   }
 
   getUser(): void {
-    this.patientapiservice.getPatients(this.pagination).subscribe((data: any) => {
-      // console.log(data);
-      this.data = data;
-      this.filteredPatient = this.data;
-    });
+
+    try {
+      this.patientapiservice.getPatients(this.pagination).subscribe((data: any) => {
+        // console.log(data);
+        this.data = data;
+        this.filteredPatient = this.data;
+      });
+    } catch (error) {
+      console.log('Error while getting users: ',error);
+    }
+
   }
 
   modifyPatient(dni: string): void {
+
+    // SHARE DATA TO MODIFY-PATIENT COMPONENT
+
     this.modifyUserService.shareData(dni);
-    this.router.navigate(['/modify-patient']);
+    this.router.navigate(['/patients/modify-patient', dni]);
   }
 
   newPatient(): void {
     this.router.navigate(['/create-patient']);
   }
 
-  filterPatients(name: string): void {
-    if (!name) {
+  filterPatients(dataToSearch: string): void {
+
+    if (!dataToSearch) {
       this.filteredPatient = this.data;
     } else {
-      this.filteredPatient = this.data.filter((dataPatientToFilter: any) =>
-        dataPatientToFilter.firstname.toLowerCase().includes(name.toLowerCase())
+      const searchName = this.data.filter((dataPatientToFilter: any) =>
+        dataPatientToFilter.firstname.toLowerCase().includes(dataToSearch.toLowerCase()) ||
+        dataPatientToFilter.lastname.toLowerCase().includes(dataToSearch.toLowerCase()) ||
+        dataPatientToFilter.dni.toLowerCase().includes(dataToSearch.toLowerCase()) ||
+        dataPatientToFilter.city.toLowerCase().includes(dataToSearch.toLowerCase()) ||
+        dataPatientToFilter.email.toLowerCase().includes(dataToSearch.toLowerCase())
+
       );
+
+      if (searchName.length > 0) {
+        this.filteredPatient = searchName;
+      } else {
+        this.filteredPatient = this.data;
+
+        Swal.fire({
+          title: 'Search error',
+          text: "We didn't found any patients..." ,
+          icon: 'error',
+        });
+      }
+
     }
   }
 
