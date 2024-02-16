c7f05ff4c8242dc30979a2c12d84dd9ffa58020f
Sam <sergioavilamacho12@gmail.com>
2024-02-16 22:14:00 +0100

    Create validator to check text format

---
+++ src/app/shared/validators/text.validator.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function textValidator(control: AbstractControl): ValidationErrors | null {
+    const regex = new RegExp('^[a-zA-ZÀ-ÿ ]+$');
+
+    const checkTextFormat = regex.test(control.value);
+
+    return checkTextFormat ? null : { invalidTextFormat: true};
+}
