981f78b7ed7a65a50990f16d08773fba17ddcb16
D4souls <sergioavilamacho12@gmail.com>
2024-03-02 18:28:35 +0100

    🪄 Added view transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes, withViewTransitions()), provideHttpClient(), provideAnimations()]
 };
