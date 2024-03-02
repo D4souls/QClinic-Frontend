738d0caa4300f77a8ebc48e9684239878741653a
D4souls <sergioavilamacho12@gmail.com>
2024-03-02 16:56:24 +0100

    ⚙️ Configured modal to show appointments details & formatted code

--- src/app/pages/appointments/show-appointments/appointments.component.ts
+++ src/app/pages/appointments/show-appointments/appointments.component.ts
@@ -5,14 +5,13 @@ import { ApiService } from '../../../core/services/api.service';
 
 // FULLCALENDAR MODULS
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions  } from '@fullcalendar/core';
+import { CalendarOptions, DateSelectArg, EventClickArg  } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import esLocale from '@fullcalendar/core/locales/es';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
-// SWEETALERTS
-import Swal from 'sweetalert2';
+import { Modal, ModalOptions, ModalInterface, InstanceOptions } from 'flowbite';
 
 @Component({
   selector: 'app-appointments',
@@ -45,7 +44,7 @@ export class AppointmentsComponent implements OnInit {
     nowIndicator: true,
     locale: esLocale,
     height: 725,
-    editable: true,
+    editable: false,
     headerToolbar: {
       start: 'prev,next today',
       center: 'title',
@@ -62,18 +61,93 @@ export class AppointmentsComponent implements OnInit {
         dayMaxEventRows: 4
       }
     },
+    eventClick(info: EventClickArg) {
+
+      // Get all modal parameters to put data
+      const $targetEl = document.getElementById('show-info-appointment');
+      const titleModal = document.getElementById('modal-header');
+      const thNamePatient = document.getElementById('th-namePatient');
+      const thDateTime = document.getElementById('th-dateTime');
+      const thNameDoctor = document.getElementById('th-nameDoctor');
+      const thComment = document.getElementById('th-comment');
+
+      if ($targetEl && titleModal && thNamePatient && thDateTime && thNameDoctor && thComment){
+
+        // Format title
+        const title = `Info appointment #${info.event.id}`;
+        
+        // Format dateTime
+        const startDate = info.event.start ? new Date(info.event.start) : null;
+        const formattedDateTime = startDate ? `${startDate.toLocaleDateString()} ${startDate.toLocaleTimeString()}` : '';
+  
+        // Configure text
+        titleModal!.innerHTML = title;
+        thNamePatient!.innerHTML = info.event.title;
+        thDateTime!.innerHTML = formattedDateTime;
+        thNameDoctor!.innerHTML = info.event.extendedProps["doctor"];
+        thComment!.innerHTML = info.event.extendedProps["description"];
+  
+        // Modal Options
+        const options: ModalOptions = {
+          placement: 'bottom-right',
+          backdrop: 'dynamic',
+          backdropClasses: 'bg-gray-900/50 fixed inset-0 z-40',
+          closable: true,
+        };
+        
+        // Modal instance options
+        const instanceOptions: InstanceOptions = {
+          id: 'show-info-appointment',
+          override: true
+        };
+    
+        const modal: Modal = new Modal($targetEl, options, instanceOptions);
+    
+        modal.show();
+
+      } else {
+        console.error("One or more elements were not found in the DOM");
+      }
+      
+    }
   };
 
-  handleDateClick(arg:any) {
-    alert('date click! ' + arg.dateStr)
+  hideModal(): void{
+    // Get modal id
+    const $targetEl = document.getElementById('show-info-appointment');
+  
+    // Modal Options
+    const options: ModalOptions = {
+      placement: 'bottom-right',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+      closable: true,
+    };
+    
+    // Modal instance options
+    const instanceOptions: InstanceOptions = {
+      id: 'show-info-appointment',
+      override: true
+    };
+
+    const modal: Modal = new Modal($targetEl, options, instanceOptions);
+  
+    modal.hide();
+
   }
+
 
   getAppointments(): void {
     this.apiGetPatient.getAppointments().subscribe((data: any) => {
       const dataEvent = data.data.map((appointments: any) => {
         return {
+          id: appointments.id,
           title: `${appointments.patientFirstname} ${appointments.patientLastname}`,
           start: appointments.date,
+          extendedProps: {
+            description: appointments.comment,
+            doctor: `${appointments.doctorFirstname} ${appointments.doctorLastname}`
+          }
         };
       });
       
