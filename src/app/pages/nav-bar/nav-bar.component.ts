4170959c52107ad9b5a037cdef97a1851457350a
D4souls <sergioavilamacho12@gmail.com>
2024-03-02 21:24:20 +0100

    ⚙️ Create toggle animation & change darkmode

--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ApiService } from '../../core/services/api.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,8 +10,26 @@ import { ApiService } from '../../core/services/api.service';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  
-  name = "Sergio"
+
+  changeMode(): void{
+
+    const darkMode = document.getElementById('moon');
+    const lightMode = document.getElementById('sun');
+    
+    document.body.classList.toggle('dark');
+
+    lightMode?.classList.toggle('animate-rotate-in');
+    darkMode?.classList.toggle('animate-rotate-in');
+    
+    if(darkMode!.style.display !== 'none'){
+      darkMode!.style.display = 'none';
+      lightMode!.style.display = 'block';
+    } else {
+      darkMode!.style.display = 'block';
+      lightMode!.style.display = 'none';
+    }
+
+  }
 
   //? NEED LOGIN DATA TO USE THIS
 
