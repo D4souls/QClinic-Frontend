0fb00e90c934abe2fd310eb921f5ded9f6703961
D4souls <sergioavilamacho12@gmail.com>
2024-03-09 01:06:13 +0100

    🔧 Configured JWTGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+import { JWTGuard } from './core/guards/JWTGuard.guard';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [JWTGuard],
     loadChildren: () => import('./pages/pages.routes').then(m => m.page_routes),
-  }
+  },
+  {
+    path: 'login',
+    title: 'Login QClinic',
+    component: LoginComponent,
+  },
 
 ];
