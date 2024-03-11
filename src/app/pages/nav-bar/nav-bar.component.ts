721498545ac25bad5eb7f1f5046234b9b18f3023
D4souls <sergioavilamacho12@gmail.com>
2024-03-11 13:55:12 +0100

    🔧 Configured animations to responsive navbar

--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+// FLOWBITE
+import { InstanceOptions, Modal, ModalOptions } from 'flowbite';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -54,6 +56,9 @@ export class NavBarComponent implements OnInit{
     const darkMode = document.getElementById('moon');
     const lightMode = document.getElementById('sun');
 
+    const darkModeMobile = document.getElementById('moon-mobile');
+    const lightModeMobile = document.getElementById('sun-mobile');
+
     const logoDark = document.getElementById('logoDark');
     const logoLight = document.getElementById('logoLight');
     
@@ -61,12 +66,18 @@ export class NavBarComponent implements OnInit{
 
     lightMode?.classList.toggle('animate-rotate-in');
     darkMode?.classList.toggle('animate-rotate-in');
+
+    lightModeMobile?.classList.toggle('animate-rotate-in');
+    darkModeMobile?.classList.toggle('animate-rotate-in');
     
     if(darkMode!.style.display !== 'none') {
       localStorage.setItem("darkModePrefer", "true");
 
       darkMode!.style.display = 'none';
       lightMode!.style.display = 'block';
+
+      darkModeMobile!.style.display = 'none';
+      lightModeMobile!.style.display = 'block';
 
       logoLight!.style.display = 'none';
       logoDark!.style.display = 'block';
@@ -77,6 +88,9 @@ export class NavBarComponent implements OnInit{
       darkMode!.style.display = 'block';
       lightMode!.style.display = 'none';
 
+      darkModeMobile!.style.display = 'block';
+      lightModeMobile!.style.display = 'none';
+
       logoLight!.style.display = 'block';
       logoDark!.style.display = 'none';
     }
@@ -84,9 +98,52 @@ export class NavBarComponent implements OnInit{
   }
 
   logout(): void {
+
+    const $targetEl = document.getElementById('mobile-navbar');
+  
+    // Modal Options
+    const options: ModalOptions = {
+      placement: 'bottom-right',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+      closable: true,
+    };
+    
+    // Modal instance options
+    const instanceOptions: InstanceOptions = {
+      id: 'new-appointment',
+      override: true
+    };
+
+    const modal: Modal = new Modal($targetEl, options, instanceOptions);
+
+    modal.hide();
+
     localStorage.removeItem('token');
     this.router.navigate(["/login"]);
     
+  }
+
+  openNavbar(): void {
+    const $targetEl = document.getElementById('mobile-navbar');
+  
+    // Modal Options
+    const options: ModalOptions = {
+      placement: 'bottom-right',
+      backdrop: 'dynamic',
+      backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+      closable: true,
+    };
+    
+    // Modal instance options
+    const instanceOptions: InstanceOptions = {
+      id: 'new-appointment',
+      override: true
+    };
+
+    const modal: Modal = new Modal($targetEl, options, instanceOptions);
+
+    modal.show();
   }
 
   //? NEED LOGIN DATA TO USE THIS
