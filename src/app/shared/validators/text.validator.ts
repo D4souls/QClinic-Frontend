026f5112a73019a0a0a180602c7e43c3946e3145
savila <savila@DES-PRTL-37>
2024-04-30 09:26:53 +0200

    🔧 Update textValidator

--- src/app/shared/validators/text.validator.ts
+++ src/app/shared/validators/text.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function textValidator(control: AbstractControl): ValidationErrors | null {
-    const regex = new RegExp('^[a-zA-ZÀ-ÿ ]+$');
+    const regex = new RegExp('^[a-zA-ZÀ-ÿ, ]+$');
 
     const checkTextFormat = regex.test(control.value);
 
