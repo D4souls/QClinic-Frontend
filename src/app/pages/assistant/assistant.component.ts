870f83a3ae714b3810be26f284d9ec6022946c95
savila <savila@DES-PRTL-37>
2024-04-30 13:59:37 +0200

    🎨 New chatBot UI

--- src/app/pages/assistant/assistant.component.ts
+++ src/app/pages/assistant/assistant.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Application } from '@splinetool/runtime';
+import { textValidator } from '../../shared/validators/text.validator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-assistant',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './assistant.component.html',
   styleUrl: './assistant.component.css'
 })
@@ -14,7 +17,52 @@ export class AssistantComponent implements OnInit{
     this.loadAssistant();
   }
 
-  chatData: any = [];
+  chatData = signal<any[]>([
+    {
+      role: "user",
+      prompt: "Hi, im Sam!",
+      date: "10:36"
+    },
+    {
+      role: "assistant",
+      prompt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae provident aspernatur animi reiciendis sit eligendi voluptates repudiandae cum maxime quod sint illum magni, beatae, voluptatem fugit atque incidunt quis accusantium!",
+      date: "10:37"
+    }
+  ]);
+
+  formAI = new FormGroup({
+    prompt: new FormControl('', [Validators.required, textValidator])
+  })
+
+  currentTimeStamp() {
+    const now = new Date();
+
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${formattedHours}:${formattedMinutes}`;
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+  }
+
+  askToAI(): void{
+    
+    const userPrompt = {
+      role: "user",
+      prompt: this.formAI.value.prompt,
+      date: this.currentTimeStamp()
+    }
+
+    this.chatData.update((messages: any) => ([...messages, userPrompt]));
+
+    // console.log(this.chatData());
+    
+  }
 
   loadAssistant(): void {
     const canvas  = document.getElementById('canvas3d') as HTMLCanvasElement;
@@ -30,6 +78,21 @@ export class AssistantComponent implements OnInit{
     if (canvas && chatAI) {
       canvas!.style.display = 'none';
       chatAI!.style.display = 'block';
+      chatAI!.classList.toggle('animate-slide-in-left');
+
+    }
+  }
+
+  closeChatBot(){
+    const canvas = document.getElementById('canvas3d');
+    const chatAI = document.getElementById('chatAI');
+
+    if (canvas && chatAI) {
+      canvas!.style.display = 'block';
+      chatAI!.style.display = 'none';
+
+      chatAI!.classList.remove('animate-slide-in-left');
+      chatAI!.classList.add('animate-slide-out-right');
     }
   }
 }
