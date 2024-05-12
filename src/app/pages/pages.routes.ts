10323e0eec641af78142324db645b5f40c4b00ed
D4souls <sergioavilamacho12@gmail.com>
2024-05-12 17:08:52 +0200

    ⚙️ Configure role guard

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { UserDataComponent } from './user-data/user-data.component';
 import { PagesComponent } from './pages.component';
+import { roleGuard } from '../core/guards/role.guard';
+import { DoctorPortalComponent } from './doctors/doctor-portal/doctor-portal.component';
 
 export const page_routes: Routes = [
   {
@@ -17,30 +18,34 @@ export const page_routes: Routes = [
         path: 'home',
         title: 'Home page',
         component: HomeComponent,
-      },
-      {
-        path: 'user-data',
-        title: 'Personal information',
-        component: UserDataComponent,
+        canActivate: [roleGuard],
       },
       {
         path: 'patients',
         title: 'Manage patients',
+        canActivate: [roleGuard],
         loadChildren: () =>
           import('./patients/patients.routes').then((m) => m.patients_routes),
       },
       {
         path: 'appointments',
         title: 'Manage appointments',
+        canActivate: [roleGuard],
         loadChildren: () =>
           import('./appointments/appointments.routes').then((m) => m.patients_routes),
       },
       {
         path: 'doctors',
         title: 'Manage doctors',
+        canActivate: [roleGuard],
         loadChildren: () =>
           import('./doctors/doctors.routes').then((m) => m.doctorsPages_routes),
       },
+      {
+        path: 'portal',
+        title: 'Doctor portal',
+        component: DoctorPortalComponent,
+      }
     ]
   },
 ];
